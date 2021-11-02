804d9188048f7678145a8ff7ac65e6eb3341726c
Gabriel <gabst138@gmail.com>
2021-11-02 19:45:40 -0300

    update bot verificação de mensagens

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 const puppeteer = require('puppeteer');
-import { selectorVisible } from './selectors';
+import selectorVisible from './selectors';
 
-async function start() {
+async function start(): Promise<void> {
   const browser = await puppeteer.launch({ 
-    headless: false, 
+    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    headless: false,
+    defaultViewport: {
+      width: 1980,
+      height: 1080,
+      deviceScaleFactor: 1,
+    },
   });
   const page = await browser.newPage();
   await page.goto('https://web.whatsapp.com/');
 
   setTimeout(async () => {
-    selectorVisible(page, browser, '[class="_1lPgH"]');
-  }, 10000);
+    selectorVisible(page, browser, '[class="_1lPgH"]', '[class="_23LrM"]');
+  }, 20000);
 };
 
 start();
--- src/selectors.ts
+++ src/selectors.ts
@@ -1,7 +1,8 @@
-async function selectorVisible(page, browser, selector_photo: string) {
+async function selectorVisible(page, browser, selector_photo: string, selector_ismsg: string) {
   const photoLogin = await page.$(selector_photo);
+  const isMsg = await page.$(selector_ismsg);
+  // VERIFICA SE ESTÁ LOGADO
   if (photoLogin) {
-    //await page.click(selector_photo);
     await page.waitForSelector(selector_photo, {visible: true});
     await page.evaluate(() => {
       return window.alert(`VOCÊ ESTÁ LOGADO!!`);
@@ -10,6 +11,17 @@ async function selectorVisible(page, browser, selector_photo: string) {
     await browser.close();
     throw ('Erro! não existe seletor ou seletor passado como argumento errado!');
   }
+
+  if (isMsg) {
+    await page.evaluate(() => {
+      return window.alert(`VOCÊ TEM UMA NOVA MENSAGEM!!`);
+    },);
+    // await page.click('[class="_3OvU8"]');
+  } else {
+    await page.evaluate(() => {
+      return window.alert(`VOCÊ NÃO POSSUI NOVAS MENSAGENS!!`);
+    },);
+  }
 };
 
-module.exports = selectorVisible;
+export default selectorVisible;
