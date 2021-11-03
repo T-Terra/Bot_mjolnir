b36da935fb2c48b0cf997b254f9164dbdf685a2f
Gabriel <gabst138@gmail.com>
2021-11-03 19:31:15 -0300

    Add: Eslint and defaultViewport: null

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,19 @@
+import { selectorVisible } from './selectors';
+
 const puppeteer = require('puppeteer');
-import selectorVisible from './selectors';
 
 async function start(): Promise<void> {
-  const browser = await puppeteer.launch({ 
-    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-    headless: false,
-    defaultViewport: {
-      width: 1980,
-      height: 1080,
-      deviceScaleFactor: 1,
-    },
-  });
-  const page = await browser.newPage();
-  await page.goto('https://web.whatsapp.com/');
+	const browser = await puppeteer.launch({
+		executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+		headless: false,
+		defaultViewport: null
+	});
+	const page = await browser.newPage();
+	await page.goto('https://web.whatsapp.com/');
 
-  setTimeout(async () => {
-    selectorVisible(page, browser, '[class="_1lPgH"]', '[class="_23LrM"]');
-  }, 20000);
-};
+	setTimeout(async () => {
+		selectorVisible(page, browser, '[class="_1lPgH"]', '[class="_23LrM"]');
+	}, 20000);
+}
 
 start();
--- src/selectors.ts
+++ src/selectors.ts
@@ -1,27 +1,20 @@
-async function selectorVisible(page, browser, selector_photo: string, selector_ismsg: string) {
-  const photoLogin = await page.$(selector_photo);
-  const isMsg = await page.$(selector_ismsg);
-  // VERIFICA SE ESTÁ LOGADO
-  if (photoLogin) {
-    await page.waitForSelector(selector_photo, {visible: true});
-    await page.evaluate(() => {
-      return window.alert(`VOCÊ ESTÁ LOGADO!!`);
-    },);
-  } else {
-    await browser.close();
-    throw ('Erro! não existe seletor ou seletor passado como argumento errado!');
-  }
+export async function selectorVisible(page, browser, selector_photo: string, selector_ismsg: string) {
+	const photoLogin = await page.$(selector_photo);
+	const isMsg = await page.$(selector_ismsg);
+	// VERIFICA SE ESTÁ LOGADO
+	if (photoLogin) {
+		await page.waitForSelector(selector_photo, { visible: true });
+		await page.evaluate(() => window.alert('VOCÊ ESTÁ LOGADO!!'));
+	} else {
+		await browser.close();
+		throw ('Erro! não existe seletor ou seletor passado como argumento errado!');
+	}
 
-  if (isMsg) {
-    await page.evaluate(() => {
-      return window.alert(`VOCÊ TEM UMA NOVA MENSAGEM!!`);
-    },);
-    // await page.click('[class="_3OvU8"]');
-  } else {
-    await page.evaluate(() => {
-      return window.alert(`VOCÊ NÃO POSSUI NOVAS MENSAGENS!!`);
-    },);
-  }
-};
-
-export default selectorVisible;
+	// VERIFICA SE TEM MENSAGEM
+	if (isMsg) {
+		await page.evaluate(() => window.alert('VOCÊ TEM UMA NOVA MENSAGEM!!'));
+		// await page.click('[class="_3OvU8"]');
+	} else {
+		await page.evaluate(() => window.alert('VOCÊ NÃO POSSUI NOVAS MENSAGENS!!'));
+	}
+}
