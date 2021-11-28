436f4cdce09634579b729b0a78a0042edef3cc5e
Gabriel <gabst138@gmail.com>
2021-11-28 14:34:53 -0300

    screenshot add

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,14 @@ export async function start(typeBot: number) {
 		const browser = await puppeteer.launch({
 			executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
 			headless: true,
-			args: ['--disable-dev-shm-usage', '--no-sandbox']
+			defaultViewport: null,
+			args: ['--disable-dev-shm-usage', '--no-sandbox', '--start-maximized']
 		});
 		const page = await browser.newPage();
+		await page.setViewport({ 
+			width: 1920,
+			height: 941
+		});
 		if (typeBot === 1) {
 			await page.goto(env.url);
 			setInterval(() => {
@@ -21,9 +26,16 @@ export async function start(typeBot: number) {
 			}, 18000);
 		} else if (typeBot === 2) {
 			await page.goto(env.urlFatec);
-			console.log('ENTROU NO SITE');
 			await sleep(3);
 			await page.click('[class="feature-circle fa fa-clock-o"]');
+			await sleep(3);
+			const segButton = await page.$x('//a[contains(text(), "SEG NOTURNO")]');
+			await sleep(2);
+			await segButton[0].click();
+			await sleep(2);
+			await page.screenshot({ path: 'img\\HoráriosFatec.png' });
+			await sleep(1);
+			await browser.close();
 		}
 	} else {
 		const browser = await puppeteer.launch({
@@ -47,7 +59,14 @@ export async function start(typeBot: number) {
 			await page.goto(env.urlFatec);
 			await sleep(3);
 			await page.click('[class="feature-circle fa fa-clock-o"]');
-			
+			await sleep(3);
+			const segButton = await page.$x('//a[contains(text(), "SEG NOTURNO")]');
+			await sleep(2);
+			await segButton[0].click();
+			await sleep(2);
+			await page.screenshot({ path: 'img\\HoráriosFatec.png' });
+			await sleep(1);
+			await browser.close();
 		}
 	}
 	
